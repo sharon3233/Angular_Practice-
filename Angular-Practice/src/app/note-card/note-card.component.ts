64eeccbaef1155ad7e394e287ee9f7b0a7b0abbe
sharon3233 <shalaskin29@gmail.com>
2022-02-08 00:48:58 -0600

    commit with blockers in note-card.components.ts

--- Angular-Practice/src/app/note-card/note-card.component.ts
+++ Angular-Practice/src/app/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @Input('title') title: string;
+  @Input('body') body: string;
 
-  ngOnInit(): void {
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    //work out if there is a text overflow and if not, then hide the truncator 
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      //if there is a text overflow, show the fade truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block')
+
+    } else {
+      // else (there is a text overflow), hide the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none')
+    }
+
   }
 
 }
